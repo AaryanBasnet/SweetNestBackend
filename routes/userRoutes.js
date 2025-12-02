b3019f8837a1465f442ed08f07084d9c3bf98349
AaryanBasnet <basnetaryan1011@gmail.com>
2025-12-02 18:29:08 +0545

    Create user routes and connect to controllers

---
+++ routes/userRoutes.js
@@ -0,0 +1,22 @@
+// routes/userRoutes.js
+const express = require('express');
+const router = express.Router();
+const {
+  registerUser,
+  loginUser,
+  getUserProfile,
+  updateUserProfile,
+} = require('../controller/userController');
+const { protect } = require('../middleware/authMiddleware');
+
+// Public routes
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+
+// Protected routes (requires JWT)
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+
+module.exports = router;
